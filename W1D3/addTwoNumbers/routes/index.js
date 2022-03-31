5cc659ed6333ccb34455686c1f980b6b5c217ab6
Aregawi-Teame <aregawiyeheys@gmail.com>
2022-03-30 21:29:36 -0500

    updated

--- W1D3/addTwoNumbers/routes/index.js
+++ W1D3/addTwoNumbers/routes/index.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const calcController = require("../controllers/calcController")
 
 
-
+router.get('/add/:num1',(req,res)=>{
+    res.status(200).json({"Result": calcController.add(req)});
+})
 module.exports = router;
